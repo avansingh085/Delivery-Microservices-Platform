dbdd26f7d77a388c743d7136810b777e85eccc68
Avan <avansingh085@gmail.com>
2026-04-19 17:12:10 +0530

    update:kafka-image and node package

--- backend/user-service/config/server.config.js
+++ backend/user-service/config/server.config.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 export const {
-PORT,
+PORT=3000,
 JWT_REFRESH_EXPIRES,
 JWT_ACCESS_EXPIRES,
 MONGO_URI,
@@ -9,5 +9,5 @@ JWT_ACCESS_SECRET,
 JWT_REFRESH_SECRET,
 REDIS_PASS,
 REDIS_ENDPOINT
-
 }=process.env;
+
